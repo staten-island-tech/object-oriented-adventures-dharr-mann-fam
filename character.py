9de8b1f79d16298e61b448466a46cc8272c27e4d
CColin27 <158204415+CColin27@users.noreply.github.com>
2024-04-08 12:15:51 -0400

    k

--- character.py
+++ character.py
@@ -1,7 +1,7 @@
 from weapon import Weapon
 
 class Character():
-    def __init__(self, name: str, health: int, damage: int, classes: str, level: int, exp: int):
+    def __init__(self, name: str, health: int, damage: int, classes: str, level: int, exp: int, exp_next: int):
         self.name = name
         self.health = health
         self.health_max = health
@@ -9,6 +9,8 @@ class Character():
         self.classes = classes
         self.level = level
         self.exp = exp
+        self.exp_next = exp_next
+
 
 
     def attack(self, target):
@@ -23,12 +25,24 @@ class Hero(Character):
                  damage: int,
                  classes: str,
                  level: int,
-                 exp: int):
-        super().__init__(name = name, health = health, damage = damage, classes = classes, level = level, exp = exp)
+                 exp: int,
+                 exp_next: int):
+        super().__init__(name = name, health = health, damage = damage, classes = classes, level = level, exp = exp, exp_next = exp_next)
+
+    
+    def exp_gain(self, exp):
+        if Enemy.health == 0:
+            Hero.exp == exp + Enemy.exp
+
+    def levelup(self, exp):
+        if Hero.exp >= Hero.exp_next:
+            Hero.exp - Hero.exp_next
+            Hero.level = Hero.level + 1
+            Hero.exp_next = Hero.exp_next * 1.5
 
 
 
 class Enemy(Character):
     def __init__(self, 
-                 name: str, health: int, damage: int, classes: str, level: int, exp: int):
-        super().__init__(name = name, health = health, damage = damage, classes = classes, level = level, exp = exp)
+                 name: str, health: int, damage: int, classes: str, level: int, exp: int, exp_next: int):
+        super().__init__(name = name, health = health, damage = damage, classes = classes, level = level, exp = exp, exp_next = exp_next)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from character import Hero, Enemy
+import json
+import os
 Y = 0
 N = 1
 
@@ -6,19 +8,19 @@ Creation = input("Enter your Username: ")
 Class_Chooser = input("Enter class [Warrior,Archer,Assassin]: ")
 if Class_Chooser == "Warrior":
     Class = "Warrior"
-    hero = Hero(name = Creation, health = 200, damage = 15, classes = "Warrior", level = 1, exp = 0 )
+    hero = Hero(name = Creation, health = 200, damage = 15, classes = "Warrior", level = 1, exp = 0, exp_next = 100)
 
 if Class_Chooser == "Archer":
     Class = "Archer"
-    hero = Hero(name = Creation, health = 100, damage = 25, classes = "Archer", level = 1, exp = 0 )
+    hero = Hero(name = Creation, health = 100, damage = 25, classes = "Archer", level = 1, exp = 0, exp_next = 100)
 
 if Class_Chooser == "Assassin":
     Class = "Assassin"
-    hero = Hero(name = Creation, health = 100, damage = 40, classes = "Assassin", level = 1, exp = 0 )
+hero = Hero(name = Creation, health = 100, damage = 40, classes = "Assassin", level = 1, exp = 0, exp_next = 100)
 
 
 
-Tutorial_Dummy = Enemy(name = "Dummy", health = 50, damage = 5, classes = "Tutorial", level = 1, exp = 100)
+Tutorial_Dummy = Enemy(name = "Dummy", health = 50, damage = 5, classes = "Tutorial", level = 1, exp = 100, exp_next = 1000)
 
 Tutorial = input("Would you like a tutorial?[Y/N]: ")
 if Tutorial == "Y":
@@ -42,3 +44,30 @@ if Tutorial == "Y":
         if Tutorial_Dummy.health == 0:
             hero.exp = hero.exp + Tutorial_Dummy.exp
             print("Congrats yours getting pretty good at this. Time for another challenge")
+
+add_data = Hero(name=Creation, health = hero.health, damage = hero.health, classes = Class_Chooser, level = hero.level, exp = hero.exp, exp_next = hero.exp_next)
+
+
+with open("data.json", "r") as f:
+    # Serialize the updated Python list to a JSON string
+    data = json.load(f)
+    ##Call classes in here
+
+
+data.append(add_data.__dict__)
+
+
+
+#No code needed below this line
+# Creates a new JSON file with the updated data
+new_file = "updated.json"
+with open(new_file, "w") as f:
+    # Serialize the updated Python list to a JSON string
+    json_string = json.dumps(data)
+
+    # Write the JSON string to the new JSON file
+    f.write(json_string)
+
+# Overwrite the old JSON file with the new one
+os.remove("data.json")
+os.rename(new_file, "data.json")
