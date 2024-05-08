0bd91ff522f0bfbc155f2ed1a0d49fd4ad0a7bc5
nathanl65 <158204596+nathanl65@users.noreply.github.com>
2024-05-08 12:29:29 -0400

    Update game.py

--- game.py
+++ game.py
@@ -24,8 +24,15 @@ Wolves = Enemy(name="Wolf", health=50, health_max=65, damage=10, classes="mob", 
 Eilte_Wolf = Boss(name="Elite Wolf", health=250, health_max=250, damage = 40, classes = "boss", level = 10, exp=150, statpoints=0, exp_next=0)
 
 #Dungeon 3
-Placeholder = Enemy(name="PlaceHolder", health = 150, health_max=150, damage=25, classes="mob", level = 10, exp = 100, statpoints=0, exp_next=0)
-PlaceholderBoss = Boss(name="PlaceHolder Boss", health=500, health_max=500, damage=60, classes="boss", level = 20, exp = 300, statpoints=0, exp_next=0 )
+Zombie = Enemy(name="Zombie", health = 250, health_max=250, damage=45, classes="mob", level = 15, exp = 750, statpoints=0, exp_next=0)
+Giant_Zombie = Boss(name="Giant_Zombie", health=500, health_max=500, damage=60, classes="boss", level = 20, exp = 300, statpoints=0, exp_next=0)
+
+#Dungeon 4 
+Spider = Enemy(name="Spider", health = 150, health_max=150, damage=25, classes="mob", level = 15, exp = 50, statpoints=0, exp_next=0)
+Tarantula = Boss(name="Tarantula", health=750, health_max=750, damage= 75, classes="boss", level = 20, exp = 300, statpoints=0, exp_next=0)
+
+#Dungeon 5
+Whalen = Boss(name="Mr. Whalen", health=1000, health_max=1000, damage= 100, classes="boss", level = 50, exp = 300, statpoints=0, exp_next=0)
 
 def battle():
         number_mobs = 10
@@ -148,10 +155,23 @@ if Choice == "1":
             number_mobs = 10
             battle()
 
+        ##Dungeon 3
         if Narration == "3":
-            Enemy = Placeholder
-            Boss = PlaceholderBoss
-            number_mobs = 10
+            Enemy = Zombie
+            Boss = Giant_Zombie
+            number_mobs = 1
+            battle()
+
+        ##Dungeon 4
+        if Narration == "4":
+            Enemy = Spider
+            Boss = Tarantula
+            number_mobs = 20
+            battle()
+
+        ##Dungeon 5
+        if Narration == "5":
+            Boss = Whalen
             battle()
 
 if Choice == "2":
